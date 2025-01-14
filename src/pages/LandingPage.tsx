046eba54722e48a582c5e55b4295c3d797658158
Vaishnavi <vaishnavip276@gmail.com>
2025-01-15 03:15:11 +0530

    landingpage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,6 @@
 import LandingNav from "@/components/LandingNav";
 import { Button } from "@/components/ui/button";
+import { Sparkles } from 'lucide-react';
 
 const LandingPage = () => {
   return <div className="bg-[#090B0D] h-screen relative">
@@ -11,6 +12,10 @@ const LandingPage = () => {
     <div className="flex flex-col items-center relative justify-center z-10">
       <LandingNav/>
         <div className="mt-36 flex flex-col items-center justify-center">
+        <Button className="custom-get-started-button" style={{borderRadius:'28px', height:"38px"}}>
+            <Sparkles className="h-8 w-8"/>
+            <span>New Task</span>
+        </Button>
         <h1 className="text-white text-4xl font-bold">Welcome to the Landing Page</h1>
         <p className="text-gray-300 mt-4 text-lg">
           This content will appear over the background image.
